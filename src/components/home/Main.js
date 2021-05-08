17a8093ac7248e975b466a61791e981cb7cdbd75
wglee0511 <69962377+wglee0511@users.noreply.github.com>
2021-05-08 16:37:07 +0900

    Add snapshot

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { LoginUserContext } from "../../context/LoginUserContext";
 import { firebaseFirestore } from "../../firebaseInit";
 
@@ -7,7 +7,19 @@ const Main = () => {
   const [textRwitts, setTextRwitts] = useState(null);
   const LoginUserValue = useContext(LoginUserContext);
   const {currentUserInfo} = LoginUserValue
-  console.log(currentUserInfo);
+
+  useEffect( () => {
+    firebaseFirestore.collection("rwitts").onSnapshot(eachSnapShot => {
+      const rwittArr = eachSnapShot.docs.map(each => {
+        return {
+          id: each.id,
+          ...each.data()
+        }
+      });
+      setTextRwitts(rwittArr);
+    })
+  },[])
+
   const onRrittSubmit = async (event) => {
     event.preventDefault();
     await firebaseFirestore.collection("rwitts").add({
@@ -28,6 +40,11 @@ const Main = () => {
       <input type="text" placeholder="무슨 일이 일어나고 있나요?"  onChange={onChangeRwitt} value={textRwitt} />
       <button>르윗</button>
     </form>
+    {textRwitts?.map(each => {
+      return (
+        <div key={each.id}>{each.text}</div>
+      )
+    })}
   </div>);
 };
 
