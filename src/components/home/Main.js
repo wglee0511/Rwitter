fed04d8cce85dfcad1963e83fd978ea40113221a
wglee0511 <69962377+wglee0511@users.noreply.github.com>
2021-05-08 17:09:58 +0900

    Update delete and update buttons

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -1,18 +1,31 @@
 import React, { useContext } from "react";
+import { LoginUserContext } from "../../context/LoginUserContext";
 import { RwittContext } from "../../context/RwittContext";
 
 const Main = () => {
   const rwittValue = useContext(RwittContext);
+  const LoginUserValue = useContext(LoginUserContext);
+  const {currentUserInfo} = LoginUserValue;
+  const currentUid = currentUserInfo.uid;
   const {onRrittSubmit,onChangeRwitt,textRwitt,textRwitts} = rwittValue;
   
+  console.log(textRwitts);
   return (<div>
     <form onSubmit={onRrittSubmit}>
-      <input type="text" placeholder="무슨 일이 일어나고 있나요?"  onChange={onChangeRwitt} value={textRwitt} />
+      <input type="text" placeholder="무슨 일이 일어나고 있나요?"  onChange={onChangeRwitt} value={textRwitt} required />
       <button>르윗</button>
     </form>
     {textRwitts?.map(each => {
       return (
-        <div key={each.id}>{each.text}</div>
+        <div key={each.id}>
+          <div>{each.text}</div>
+          {currentUid === each.userId && 
+          <>
+          <button>수정</button>
+          <button>삭제</button>
+          </>
+        }
+        </div>
       )
     })}
   </div>);
