dd73a9ceed861348272c9308c0caa71670a48955
wglee0511 <ranazang@naver.com>
2021-05-11 21:38:19 +0900

    Add desc

--- src/context/RwittContext.js
+++ src/context/RwittContext.js
@@ -1,6 +1,6 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import { firebaseFirestore } from '../firebaseInit';
-import { LoginUserContext } from './LoginUserContext';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { firebaseFirestore } from "../firebaseInit";
+import { LoginUserContext } from "./LoginUserContext";
 
 const RwittContext = createContext();
 
@@ -8,47 +8,47 @@ const RwittContextProvider = (props) => {
   const [textRwitt, setTextRwitt] = useState("");
   const [textRwitts, setTextRwitts] = useState(null);
   const LoginUserValue = useContext(LoginUserContext);
-  const {currentUserInfo} = LoginUserValue;
+  const { currentUserInfo } = LoginUserValue;
 
-  useEffect( () => {
-    firebaseFirestore.collection("rwitts").onSnapshot(eachSnapShot => {
-      const rwittArr = eachSnapShot.docs.map(each => {
+  useEffect(() => {
+    firebaseFirestore.collection("rwitts").onSnapshot((eachSnapShot) => {
+      firebaseFirestore.collection("rwitts").orderBy("creatAt", "desc");
+      const rwittArr = eachSnapShot.docs.map((each) => {
         return {
           id: each.id,
-          ...each.data()
-        }
+          ...each.data(),
+        };
       });
       setTextRwitts(rwittArr);
-    })
-  },[])
+    });
+  }, []);
 
   const onRrittSubmit = async (event) => {
     event.preventDefault();
     await firebaseFirestore.collection("rwitts").add({
-      userId : currentUserInfo.uid,
-      creatAt : Date.now(),
-      text: textRwitt
-    })
+      userId: currentUserInfo.uid,
+      creatAt: Date.now(),
+      text: textRwitt,
+    });
     setTextRwitt("");
-  }
+  };
 
   const onChangeRwitt = (event) => {
     const text = event.target.value;
     setTextRwitt(text);
-  }
-  
+  };
 
-    const rwittValue = {
-        textRwitt,
-        textRwitts,
-        onRrittSubmit,
-        onChangeRwitt
-    }
-    return (
-        <RwittContext.Provider value={rwittValue}>
-            {props.children}
-        </RwittContext.Provider>
-    )
-}
+  const rwittValue = {
+    textRwitt,
+    textRwitts,
+    onRrittSubmit,
+    onChangeRwitt,
+  };
+  return (
+    <RwittContext.Provider value={rwittValue}>
+      {props.children}
+    </RwittContext.Provider>
+  );
+};
 
-export {RwittContextProvider, RwittContext}
+export { RwittContextProvider, RwittContext };
